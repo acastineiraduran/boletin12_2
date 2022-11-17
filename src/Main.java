40d42c0cd5a544a08a4ed48c27666743fdd01a1b
acastineiraduran <acastineiraduran@danielcastelao.org>
2022-11-17 22:42:35 +0100

    modificacion de la main con los metodos

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //me falta poner el numero maximo de intentos
+
+        Metodos obx = new Metodos();
+
+        String mensajeIntentos = "Tienes 10 intentos";
+        String mensajeRango = "Escribe un numero entre 1 y 50";
+        String mensajeAcierto = "Has acertado el numero";
+        int numeroCorrecto = obx.claseRandom(1, 50);
+        System.out.println(numeroCorrecto);
+
+        int numeroJugador;
+        System.out.println(mensajeIntentos);
+        do {
+            numeroJugador = obx.pedirNumero(mensajeRango);
+            if(numeroCorrecto != numeroJugador){
+                obx.pista();
+            }
+            else {
+                System.out.println(mensajeAcierto);
+            }
+
+        } while (numeroCorrecto != numeroJugador);
+        System.out.println("programa finalizado");
     }
 }
